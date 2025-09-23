d2627324b8d76cc04aeed273e221054316998bdb
Ahmed Mohamed Fouad <63275575+ahmedfouad01099@users.noreply.github.com>
2025-09-23 17:30:49 +0300

    finish folder structure setup

---
+++ ThreadsCloneSwiftUI/ThreadsCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneSwiftUIApp.swift
+//  ThreadsCloneSwiftUI
+//
+//  Created by Ahmed Fouad on 23/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThreadsCloneSwiftUITests/ThreadsCloneSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneSwiftUITests.swift
+//  ThreadsCloneSwiftUITests
+//
+//  Created by Ahmed Fouad on 23/09/2025.
+//
+
+import Testing
+@testable import ThreadsCloneSwiftUI
+
+struct ThreadsCloneSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
