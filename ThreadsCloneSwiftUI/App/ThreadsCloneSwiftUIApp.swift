d4b307bb9654378e94e674ad4a292da6afb9ea42
Ahmed Mohamed Fouad <63275575+ahmedfouad01099@users.noreply.github.com>
2025-09-28 02:19:50 +0300

    finish firebase setup

---
+++ ThreadsCloneSwiftUI/App/ThreadsCloneSwiftUIApp.swift
@@ -0,0 +1,31 @@
+//
+//  ThreadsCloneSwiftUIApp.swift
+//  ThreadsCloneSwiftUI
+//
+//  Created by Ahmed Fouad on 23/09/2025.
+//
+
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+
+@main
+struct ThreadsCloneSwiftUIApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
