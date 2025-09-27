dad700809a4d1d1ffce5b4e18d4156bdd3338ff1
Ahmed Mohamed Fouad <63275575+ahmedfouad01099@users.noreply.github.com>
2025-09-27 21:20:08 +0300

    finish the design of edit profile view

---
+++ ThreadsCloneSwiftUI/Core/Profile/EditProfile/View/EditProfileView.swift
@@ -0,0 +1,105 @@
+//
+//  EditProfileView.swift
+//  ThreadsCloneSwiftUI
+//
+//  Created by Ahmed Fouad on 27/09/2025.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    @State private var bioText = ""
+    @State private var link = ""
+    @State private var isPrivateProfile: Bool = false
+
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color(.systemGroupedBackground)
+                    .edgesIgnoringSafeArea([.bottom, .horizontal])
+
+                VStack {
+                    // name and profile
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("Name")
+                                .fontWeight(.semibold)
+
+                            Text("Ahmed Fouad")
+                        }
+
+                        Spacer()
+
+                        CircularProfileImageView()
+                    }
+
+                    Divider()
+
+                    // bio field
+                    VStack(alignment: .leading) {
+                        Text("Bio")
+                            .fontWeight(.semibold)
+
+                        TextField(
+                            "Enter your bio..",
+                            text: $bioText,
+                            axis: .vertical
+                        )
+                    }
+
+                    Divider()
+
+                    // link field
+                    VStack(alignment: .leading) {
+                        Text("Link")
+                            .fontWeight(.semibold)
+
+                        TextField(
+                            "Add link...",
+                            text: $link,
+                        )
+                    }
+
+                    Divider()
+
+                    Toggle("Private profile", isOn: $isPrivateProfile)
+                }
+                .font(.footnote)
+                .padding()
+                .background(.white)
+                .padding()
+                .overlay {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color(.systemGray4), lineWidth: 1)
+                }
+                .cornerRadius(10)
+
+            }
+            .navigationTitle("Edit Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(.black)
+                }
+
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Done") {
+
+                    }
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.black)
+
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
